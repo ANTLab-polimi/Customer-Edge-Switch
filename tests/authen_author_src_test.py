41fd3489bd80f19287d9216bc849b69573c22deb
lucajako97 <luca2.giacometti@mail.polimi.it>
2022-11-23 18:01:39 +0100

    updating the authen test src with a socket client to interact with the server

--- tests/authen_author_src_test.py
+++ tests/authen_author_src_test.py
@@ -12,6 +12,8 @@ dst_ip = "192.168.56.6"
 iface = 'eth0'
 auth_port = 101
 http_port = 80
+protocol = "TCP"
+method = "ip"
 
 def netcat(hostname, port, content, flag):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -40,17 +42,48 @@ class MyEncoder(JSONEncoder):
     def default(self, obj):
         return obj.__dict__
 
-auth = Auth("192.169.56.2", "ip", self_ip, 80, "TCP", "302130123456789", 1, 1.0)
+ismi = "302130123456789"
+count = 1
+version = 1.0
+
+# starting the authentication phase
+
+auth = Auth(dst_ip, method, self_ip, http_port, protocol, ismi, count, version)
 #auth = Auth("192.169.56.2", "imsi", "111111111111111", 80, "TCP", "111111111111111", 1, 1.0)
 #auth = Auth("192.169.56.2", "token", "abcdefghilmnopqrstuvz", 80, "TCP", "111111111111111", 1, 1.0)
 auth = MyEncoder().encode(auth)
+
+# key exchanging
+# TODO dh needs a parameterization for the IPs and ports...
+
 key = dh("302130123456789")
+
+# continuing the authentication phase, encoding the message and hmac with the dh key
+
 message_bytes = auth.encode('ascii')
 base64_bytes = base64.b64encode(message_bytes)
 hmac_hex = hmac.new(bytes(key, 'utf-8'), base64_bytes, hashlib.sha512).hexdigest()
 msg = str(base64_bytes) + '---' + str(hmac_hex)
 
+# sending the authentication message to the controller
 netcat(controller_ip, auth_port, bytes(msg, 'utf-8'), True)
 time.sleep(0.1)
+
+# open a socket to comunicate with the server:
+
 print("Requesting connection with server...")
-netcat(dst_ip, http_port, b"Hello Helium!", False)
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((dst_ip, http_port))
+i = 1
+while True:
+    time.sleep(1)
+    s.senddall(b"Hello world!" + bytes(i))
+    data = s.recv(1024)
+    if not data:
+        print("no data :(")
+        break
+    else:
+        print(f"Received: {data!r}")
+    i = i + 1
+
+#netcat(dst_ip, http_port, b"Hello Helium!", False)
